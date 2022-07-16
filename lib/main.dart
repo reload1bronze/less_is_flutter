33a8ee8b821268d7e19ffa8526727f14b58d5db9
reload1bronze <nature9th@gmail.com>
2022-07-16 22:27:04 +0900

    feat: 리스트뷰아이템 클릭시 해당 아이템의 상태만 변경하기

--- lib/main.dart
+++ lib/main.dart
@@ -2,31 +2,80 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    print('부모 위젯 빌드');
     return MaterialApp(
       // home: ShoppingPage(),
-      home: Scaffold(
-        body: Container(
-          child: Center(
-            child: IconButton(
-                icon: FaIcon(
-                  FontAwesomeIcons.gamepad,
-                  color: Colors.redAccent,
-                  size: 100,
+      home: SafeArea(
+        child: Scaffold(
+          body: Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                    padding: const EdgeInsets.all(8),
+                    itemCount: 10,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListViewItem(index);
+                    }),
+              ),
+              Container(
+                child: Center(
+                  child: IconButton(
+                      icon: FaIcon(
+                        FontAwesomeIcons.gamepad,
+                        color: Colors.redAccent,
+                        size: 100,
+                      ),
+                      onPressed: () {
+                        print("Pressed");
+                      }),
                 ),
-                onPressed: () {
-                  print("Pressed");
-                }),
+              ),
+            ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class ListViewItem extends StatefulWidget {
+  final int index;
+
+  ListViewItem(this.index, {Key? key}) : super(key: key);
+
+  @override
+  State<ListViewItem> createState() => _ListViewItemState();
+}
+
+class _ListViewItemState extends State<ListViewItem> {
+  bool isSwitched = false;
+
+  @override
+  Widget build(BuildContext context) {
+    print('자식 위젯 빌드 ${widget.index}');
+    return Column(
+      children: [
+        Text(isSwitched.toString()),
+        ListTile(
+          title: new Text('gogo'),
+          trailing: new Switch(
+            value: isSwitched,
+            activeColor: Colors.pink,
+            activeTrackColor: Colors.pinkAccent,
+            onChanged: (value) {
+              setState(() {
+                isSwitched = value;
+              });
+            },
+          ),
+        ),
+      ],
     );
   }
 }
