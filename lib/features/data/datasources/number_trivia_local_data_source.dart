7159674c109e146256c80ed73abfb567a793caa0
reload1bronze <nature9th@gmail.com>
2022-07-22 22:46:55 +0900

    feat: local data source
    - 유튜브 Flutter TDD Clean Architecture Course 참고

--- lib/features/data/datasources/number_trivia_local_data_source.dart
+++ lib/features/data/datasources/number_trivia_local_data_source.dart
@@ -1,6 +1,41 @@
+import 'dart:convert';
+
+import 'package:less_is_flutter/core/error/exceptions.dart';
 import 'package:less_is_flutter/features/data/models/number_trivia_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const CACHED_NUMBER_TRIVIA = 'CACHED_NUMBER_TRIVIA';
 
 abstract class NumberTriviaLocalDataSource {
   Future<NumberTriviaModel> getLastNumberTrivia();
+
   Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache);
+}
+
+class NumberTriviaLocalDataSourceImpl implements NumberTriviaLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  NumberTriviaLocalDataSourceImpl({required this.sharedPreferences});
+
+  @override
+  Future<NumberTriviaModel> getLastNumberTrivia() {
+    final jsonString = sharedPreferences.getString(CACHED_NUMBER_TRIVIA);
+    if (jsonString != null) {
+      return Future.value(
+        NumberTriviaModel.fromJson(
+          json.decode(jsonString),
+        ),
+      );
+    } else {
+      throw CacheException();
+    }
+  }
+
+  @override
+  Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache) {
+    return sharedPreferences.setString(
+      CACHED_NUMBER_TRIVIA,
+      json.encode(triviaToCache.toJson()),
+    );
+  }
 }
